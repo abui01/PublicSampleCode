e7b7912187452e9af163b5663bfbe16b3c4a2794
abui0 <abui0@Abui-Laptop.fios-router.home>
2018-04-15 19:17:38 -0400

    Dev 2 updates

--- TheGreatCourse/src/test/java/com/learning/pages/YourShoppingCartPage.java
+++ TheGreatCourse/src/test/java/com/learning/pages/YourShoppingCartPage.java
@@ -26,7 +26,7 @@ public class YourShoppingCartPage extends BasePage{
 		WebElement pageHeaderElem = myLib.fluentWait(By.tagName("h1"));
 		System.out.println("text: " + pageHeaderElem.getText());
 		System.out.println("Fixed the Test");
-		System.out.println("Fixed the Test again!");
+		System.out.println("I am Dev 2");
 		assertEquals("Your Shopping Cart", pageHeaderElem.getText());		
 	}
 	
